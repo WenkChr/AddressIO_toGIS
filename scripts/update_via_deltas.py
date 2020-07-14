9302b45606fcf94471b9487042c7003b22e5ac57
WenkChr <christopher.wenkoff@canada.ca>
2020-07-14 13:38:31 -0400

    Download new or updates files added

--- scripts/update_via_deltas.py
+++ scripts/update_via_deltas.py
@@ -16,17 +16,27 @@ def check_url_exists(url):
         print('Fix error then restart script')
         sys.exit()
 
-def check_for_updates(url, old_data_zip):
-    # Get Creation date for the old zip
-    old_data_c_date = time.ctime(os.path.getctime(old_data_zip))
-    # Get update date from online version
-    # Explanation here: https://stackoverflow.com/questions/5710867/downloading-and-unzipping-a-zip-file-without-writing-to-disk
+def splitall(path):
+    allparts = []
+    while 1:
+        parts = os.path.split(path)
+        if parts[0] == path:  # sentinel for absolute paths
+            allparts.insert(0, parts[0])
+            break
+        elif parts[1] == path: # sentinel for relative paths
+            allparts.insert(0, parts[1])
+            break
+        else:
+            path = parts[0]
+            allparts.insert(0, parts[1])
+    return allparts
 #--------------------------------------------------------------------------------------------------------------------------------
 # inputs
 Data_GDB = 'H:\\AddressIO_Home\\workingGDB.gdb'
 OpenIO_zip = "https://data.openaddresses.io/openaddr-collected-north_america.zip"
+intermediateZipFolder = 'H:\\AddressIO_Home\\IntermediateZips'
 Old_Data = 'H:\AddressIO_Home\openaddr-collected-north_america'
-outPath = 'H:\\AddressIO_Scripts'
+outPath = 'H:\\AddressIO_Home' # This should be where the full AddressIO data should be located (unzipped)
 stateURL = r'http://results.openaddresses.io/state.txt'
 #------------------------------------------------------------------------------------------------------------------------------------
 # function calls
@@ -47,23 +57,35 @@ print('Creating dataset names from source field')
 stateDF['locationName'] = stateDF.processed.apply(lambda x: pd.Series(str(x).split('/')[-1].split('.')[0]))
 stateDF.rename(columns={'address count' : 'address_count'}, inplace=True)
 # Build counts for the existing data and then
-rows_to_download = []
+rows_to_download = [] # list of paths of zips to download
 for row in stateDF.itertuples():
     if row.locationName == 'nan':
         continue
     prov = row.source.split('/')[1]
     # If file is not in current download of the addressIO data then download it as a new file
     if not os.path.exists(os.path.join(Old_Data, 'ca', prov, row.locationName + '.csv')) and row.processed != 'nan':
-        rows_to_download += row.locationName
+        rows_to_download.append(row.processed)
     if os.path.exists(os.path.join(Old_Data, 'ca', prov, row.locationName + '.csv')):
         oldProvdf = pd.read_csv(os.path.join(Old_Data, 'ca', prov, row.locationName + '.csv'))
-        #stateDF.at[int(row.index), 'old_count'] = len(oldProvdf)
         if len(oldProvdf) != row.address_count:
             print(f'{row.locationName} does not equal old count')
             print(f'Old Count: {len(oldProvdf)} New Count: {int(row.address_count)}')
-            rows_to_download += row.locationName
+            rows_to_download.append(row.processed)
+
+# Download zips with files that need to be updated
+intZips = [] # list of paths to intermediate zips
+for zfile in rows_to_download:
+    print(f'Downloading: {os.path.split(zfile)[-1]}')
+    urlSplit = os.path.split(zfile)
+    r= requests.get(zfile, stream= True)
+    downPath = os.path.join(outPath, 'IntermediateZips', os.path.split(zfile)[-1])
+    with open( downPath, 'wb') as fd:
+        for chunk in r.iter_content(chunk_size= 128):
+            fd.write(chunk)
+    intZips.append(downPath)     
 
 
-print(stateDF.head())
+print(len(rows_to_download))
+print(rows_to_download)
 
 print('DONE!')
