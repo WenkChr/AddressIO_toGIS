283aab8d50ec3dc30bdd04d3f8fa6bb6ca27b34b
WenkChr <christopher.wenkoff@canada.ca>
2020-08-17 14:42:07 -0400

    Working version for all provinces. Ontario get special treatment due to high number of points. Save memeory, etc

--- scripts/BuildCoverage.py
+++ scripts/BuildCoverage.py
@@ -16,7 +16,8 @@ def projectFC(fc, outGDB, outName, outSpatRef):
         return os.path.join(outGDB, outName)
 
 #-----------------------------------------------------------------------------------
-#Inputs
+#Input
+homepath = r'H:\AddressIO_Home'
 csdPath = r'H:\AddressIO_Home\lcsd000b16a_e.shp'
 provFilesGDB = r'H:\AddressIO_Home\workingGDB.gdb'
 workingGDB = r'H:\AddressIO_Home\workingGDB.gdb'
@@ -27,30 +28,40 @@ t0 = time.time()
 prCodes = {'ab' : 48, 'bc' : 59, 'mb' : 46, 'nb' : 13, 'nl' : 10, 'ns' : 12, 'on' : 35, 
         'qc' : 24, 'sk' : 47, 'yt' : 60, 'pe' : 11, 'nt' : 61, 'nu' : 62}
 # Loop to create provincial spatial joins in the scratch gdb
-for prov in ('ab', 'bc', 'mb', 'nb', 'nl', 'ns', 'on', 'qc', 'sk', 'yt', 'pe', 'nt', 'nu'):
-        provfc = os.path.join(workingGDB, f'{prov}_all')
-        if not arcpy.Exists(provfc): continue #IF a file does not exist for a province then ignore it
-        print(f'Creating coverage for {prov}')
-        #csdlyr = arcpy.MakeFeatureLayer_management(csdPath, where_clause= f"PRUID = '{str(prCodes[prov])}'")
-        alyr = arcpy.MakeFeatureLayer_management(os.path.join(workingGDB, f'{prov}_all'))
-        arcpy.SpatialJoin_analysis(alyr, csdPath, os.path.join(scratchGDB, f'{prov}_sj'))
+# for prov in ('ab', 'bc', 'mb', 'nb', 'nl', 'ns', 'on', 'qc', 'sk', 'yt', 'pe', 'nt', 'nu'):
+#         provfc = os.path.join(workingGDB, f'{prov}_all')
+#         if not arcpy.Exists(provfc): continue #IF a file does not exist for a province then ignore it
+#         print(f'Creating coverage for {prov}')
+#         #csdlyr = arcpy.MakeFeatureLayer_management(csdPath, where_clause= f"PRUID = '{str(prCodes[prov])}'")
+#         alyr = arcpy.MakeFeatureLayer_management(os.path.join(workingGDB, f'{prov}_all'))
+#         arcpy.SpatialJoin_analysis(alyr, csdPath, os.path.join(scratchGDB, f'{prov}_sj'))
 count_series = []
-for prov in ('pe', 'ns', 'nl'):
+for prov in ('on', 'ab', 'bc', 'mb', 'nb', 'nl', 'ns', 'qc', 'sk', 'yt', 'pe', 'nt', 'nu'): # 'on' removed because of memory issues loading points into memory
+        if not arcpy.Exists(os.path.join(scratchGDB, f'{prov}_sj')): continue
         print(f'Getting counts for {prov}')
         fc = os.path.join(scratchGDB, f'{prov}_sj')
-        df = pd.DataFrame.spatial.from_featureclass(fc)
-        groups = df['CSDUID'].value_counts()
-        del df
-        count_series.append(groups)
-        print(len(groups))
+        if prov == 'on':
+                ids = pd.DataFrame.spatial.from_featureclass(csdPath, columns= ['PRUID', 'CSDUID'], where_clause= "PRUID = '35'")
+                for cid in ids['CSDUID'].tolist():
+                        print(f'Getting count for CSDUID: {cid}')
+                        df = pd.DataFrame.spatial.from_featureclass(fc, columns= ['CSD_UID'], where_clause= f"CSDUID = '{cid}'")
+                        groups = df['CSDUID'].value_counts()
+                        del df
+                        count_series.append(groups)
 
-print(count_series)
-csd_counts_df = pd.concat(count_series, names= ['AddressCount'])       
+        else:
+                df = pd.DataFrame.spatial.from_featureclass(fc, columns= ['CSDUID'])
+                groups = df['CSDUID'].value_counts()
+                del df
+                count_series.append(groups)
+
+csd_counts_df = pd.concat(count_series, axis= 0)       
 csddf = pd.DataFrame.spatial.from_featureclass(csdPath)
 merged = pd.merge(csddf, csd_counts_df, left_on='CSDUID', right_index= True)
 del csddf
 merged.rename(columns= {'CSDUID_y' : 'AddressCount'}, inplace= True)
-print(merged.head())
+merged.drop(['CSDUID_x', 'FID'], axis= 1, inplace= True)
+merged.spatial.to_featureclass(os.path.join(workingGDB, 'DataCoverage'), overwrite= True)
 t1 = time.time() 
 print(f'Total time taken to run script: {round((t1 - t0)/60, 2)} min')
 print('DONE!')
